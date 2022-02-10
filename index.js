ad43f894c8f9cded6f753c521b414c6349f1fcd3
samgerrick01 <desilva.sam17.sgds@gmail.com>
2022-02-10 14:18:16 +0800

    make it better3

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use("/", (req, res) => {
   res.send("Hello to memories API");
 });
 
-const PORT = process.env.PORTS || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
